1de4cc39a1231b7e83796833a456223074ceea29
andySigler <andrewsigler1@gmail.com>
2023-01-17 22:34:32 -0500

    wip: removing most input() statements

--- hardware/opentrons_hardware/scripts/96Head_tip_pick_up_test_script.py
+++ hardware/opentrons_hardware/scripts/96Head_tip_pick_up_test_script.py
@@ -1,20 +1,18 @@
 """A script for sending and receiving data from sensors on the OT3."""
-"""Andy C testint push"""
 
 import logging
 import asyncio
 import argparse
 from numpy import float64
 import termios
-import sys, tty
+import sys
+import tty
+from typing import List, Tuple
 
 from typing import Callable
 from logging.config import dictConfig
 
-from opentrons_hardware.firmware_bindings.messages.message_definitions import (
-    EnableMotorRequest,
-    WriteMotorCurrentRequest,
-)
+from opentrons_hardware.firmware_bindings.messages.message_definitions import WriteMotorCurrentRequest
 from opentrons_hardware.firmware_bindings.utils import UInt32Field
 from opentrons_hardware.firmware_bindings.messages import payloads
 from opentrons_hardware.drivers.can_bus.can_messenger import CanMessenger
@@ -29,6 +27,40 @@ from opentrons_hardware.hardware_control.motion import (
 from opentrons_hardware.hardware_control.move_group_runner import MoveGroupRunner
 
 from opentrons_hardware.drivers.can_bus.build import build_driver
+
+from opentrons_shared_data.pipette.load_data import load_definition as load_pipette_definition
+from opentrons_shared_data.pipette.pipette_definition import (
+    PipetteVersionType, PipetteChannelType, PipetteModelType, PipetteTipType,
+)
+
+AccuracyAdjustTable = List[Tuple[float, float, float]]
+# copy the lookup table from EVT single-channel pipettes
+PIPETTE_SINGLE_EVT_VERSION = PipetteVersionType(major=3, minor=3)
+PIPETTE_SINGLE_EVT_IDEAL_UL_PER_MM = 15.904
+
+
+def _get_accuracy_adjust_table(tip_volume: int) -> AccuracyAdjustTable:
+    _volume_to_tip = {50: PipetteTipType.t50, 200: PipetteTipType.t200, 1000: PipetteTipType.t1000}
+    _tip = _volume_to_tip[tip_volume]
+    _pipette = load_pipette_definition(
+        PipetteModelType.p1000,
+        PipetteChannelType.SINGLE_CHANNEL,
+        PIPETTE_SINGLE_EVT_VERSION,
+    )
+    return _pipette.supported_tips[_tip].aspirate["default"]
+
+
+def _get_plunger_distance_for_volume(table: AccuracyAdjustTable, volume: float) -> float:
+    for i in range(len(table) - 1):
+        this_entry = table[i]
+        next_entry = table[i + 1]
+        if this_entry[0] < volume < next_entry[0]:
+            tv, ts, ti = this_entry
+            print(f"\tv={tv}, s={ts}, i={ti}")
+            ul_per_mm = (volume * ts) + ti
+            mm_travel = volume / ul_per_mm
+            return mm_travel
+    raise ValueError(f"unable to find volume {volume} in table")
 
 
 GetInputFunc = Callable[[str], str]
@@ -489,108 +521,92 @@ async def run(args: argparse.Namespace) -> None:
             await asyncio.sleep(delay)
         #-------------------Pick up tip Motors----------------------------------
         elif args.test == 'full_pick_up_tip':
-            blow_out_pos = 69
-            bottom_pos = 67
-            plunger_speed = 10  ## mm/sec
-            aspirate_distance_mm = 64 #13.21  # a / ((a * b) + c)  --- using table entry ABOVE desired volume
-            mm_per_ul = aspirate_distance_mm / 200.0
-            wet_air_gap_mm = 40 * mm_per_ul  # make this ~2% of total volume
-            dry_air_gap_mm = 15 * mm_per_ul
-            dispense_distance_mm = aspirate_distance_mm + wet_air_gap_mm
+            bottom_pos = 66
+            blow_out_pos = 71
+            plunger_speed_mm = 5  # mm/sec
+            trailing_air_gap_speed_mm = 1  # mm/sec
             blow_out_plunger_distance_mm = blow_out_pos - bottom_pos
-            input('ENTER to home Z')
-            await home_z_axis().run(can_messenger = messenger)
+            vol_str = input("Enter volume for WET air-gap (submerged blow-out): ").strip()
+            if vol_str:
+                wet_air_gap_mm = float(vol_str) / PIPETTE_SINGLE_EVT_IDEAL_UL_PER_MM
+            else:
+                wet_air_gap_mm = 0
+            vol_str = input("Enter volume for DRY air-gap (carrying air-gap): ").strip()
+            if vol_str:
+                dry_air_gap_mm = float(vol_str) / PIPETTE_SINGLE_EVT_IDEAL_UL_PER_MM
+            else:
+                dry_air_gap_mm = 0
+            vol_str = input("Enter volume for ASPIRATE volume: ").strip()
+            aspirate_ul = float(vol_str)
+            tip_volume = int(input("Enter tip size (50, 200, or 1000): "))
+            table = _get_accuracy_adjust_table(tip_volume)
+            # only use accuracy-adjustment when moving liquids
+            aspirate_mm = _get_plunger_distance_for_volume(table, aspirate_ul)
+            dispense_mm = dry_air_gap_mm + aspirate_mm + wet_air_gap_mm
+            print(f"wet-air-gap-mm: {wet_air_gap_mm}")
+            print(f"aspirate-mm: {aspirate_mm}")
+            print(f"dry-air-gap-mm: {dry_air_gap_mm}")
+            print(f"total travelled: {wet_air_gap_mm + aspirate_mm + dry_air_gap_mm} / {bottom_pos}")
+            should_pre_wet = ("y" in input("Pre-wet x5 times? (y/n): "))
+            print('HOMING Z')
+            await home_z_axis().run(can_messenger=messenger)
             input("ENTER to home PLUNGER")
-            # await home_gantry.run(can_messenger = messenger)
-            # await asyncio.sleep(delay)
-            await set_current(messenger, 1.5,NodeId.pipette_left)
+            await set_current(messenger, 1.5, NodeId.pipette_left)
             await home_pipette.run(can_messenger=messenger)
             input("ENTER to home JAW")
             await home_jaw.run(can_messenger=messenger)
-            input("ENTER to jog to TIPRACK")
-            position = {'gantry_x': 0,
-                        'gantry_y': 0,
-                        'head_l': 0}
-            tiprack_pos = await _jog_axis(messenger, position, only_z=True)
-            input("ENTER to grab tips")
-            await grab_tips.run(can_messenger=messenger)
-            input("ENTER to home jaw")
-            await home_jaw.run(can_messenger=messenger)
-            input('ENTER to home Z')
-            await home_z_axis().run(can_messenger=messenger)
+            if "y" in input("Pick-up Tips? (y/n): ").lower():
+                print("JOG to the TIPRACK")
+                position = {'gantry_x': 0,
+                            'gantry_y': 0,
+                            'head_l': 0}
+                tiprack_pos = await _jog_axis(messenger, position, only_z=True)
+                print("grabbing the tips")
+                await grab_tips.run(can_messenger=messenger)
+                input("ENTER to home jaw")
+                await home_jaw.run(can_messenger=messenger)
+                input('ENTER to home Z')
+                await home_z_axis().run(can_messenger=messenger)
             # Prepare for aspirate --bottom
-            await set_current(messenger, 1.5,NodeId.pipette_left)
-            input('ENTER to prepare for aspirate')
-            await move_plunger(bottom_pos, 10).run(can_messenger = messenger)
-            #input('ENTER to add AIR-GAP')
-            #await move_plunger(wet_air_gap_mm, -10).run(can_messenger=messenger)
-            input('ENTER to jog to TROUGH')
+            await set_current(messenger, 1.5, NodeId.pipette_left)
+            print('preparing for aspirate')
+            await move_plunger(bottom_pos, 10).run(can_messenger=messenger)
+            print('aspirating LEADING-AIR-GAP')
+            await move_plunger(wet_air_gap_mm, 10).run(can_messenger=messenger)
+            print('JOG to the TROUGH')
             position = {'gantry_x': tiprack_pos['gantry_x'],
                         'gantry_y': tiprack_pos['gantry_y'],
                         'head_l': 0}
             await _jog_axis(messenger, position, only_z=True)
-            input('ENTER to Aspirate')
             # Aspirate
-            """
-            for i in range(5):
-                print(f"Pre-Wet {i + 1}/5")
-                await move_plunger(aspirate_distance_mm, -plunger_speed).run(can_messenger = messenger)
-                await move_plunger(aspirate_distance_mm, plunger_speed).run(can_messenger=messenger)
-            """   
-            print("aspirating")
-            await move_plunger(aspirate_distance_mm, -plunger_speed).run(can_messenger=messenger)
-            input('ENTER to jog to RETRACT position')
+            if should_pre_wet:
+                pre_wet_count = 5
+                for i in range(pre_wet_count):
+                    print(f"Pre-Wet {i + 1}/{pre_wet_count}")
+                    await move_plunger(aspirate_mm, -plunger_speed_mm).run(can_messenger=messenger)
+                    await move_plunger(aspirate_mm, plunger_speed_mm).run(can_messenger=messenger)
+            print(f"aspirating: {aspirate_mm} mm")
+            await move_plunger(aspirate_mm, -plunger_speed_mm).run(can_messenger=messenger)
+            print('JOG to the RETRACT position')
             await _jog_axis(messenger, position, only_z=True)
-            input('ENTER to do an AIR-GAP')
-            await move_plunger(dry_air_gap_mm, -plunger_speed).run(can_messenger=messenger)
-            input('ENTER to jog to PLATE')
+            print('doing a TRAILING-AIR-GAP')
+            await move_plunger(dry_air_gap_mm, -trailing_air_gap_speed_mm).run(can_messenger=messenger)
+            print('JOG to the DISPENSE position')
             await _jog_axis(messenger, position, only_z=True)
-            input('ENTER to remove AIR-GAP')
-            await move_plunger(dry_air_gap_mm, plunger_speed).run(can_messenger=messenger)
-            input('ENTER to jog to ASPIRATE')
+            print(f'dispensing: {dispense_mm} mm')
+            await move_plunger(dispense_mm, plunger_speed_mm).run(can_messenger=messenger)
+            print('JOG to the BLOWOUT position')
             await _jog_axis(messenger, position, only_z=True)
-            input('ENTER to DISPENSE')
-            # Dispense + Blow out
-            await move_plunger(dispense_distance_mm, plunger_speed).run(can_messenger = messenger)
-            input('ENTER to jog a BLOWOUT position')
-            await _jog_axis(messenger, position, only_z=True)
-            input('ENTER to blowout')
+            print(f'blowout: {blow_out_plunger_distance_mm} mm')
             await move_plunger(blow_out_plunger_distance_mm, 10).run(can_messenger=messenger)
-            input('ENTER to jog a bit more')
+            print('JOG to CATCH DROPLETS')
             await _jog_axis(messenger, position, only_z=True)
-            input('Press ENTER to home Z axis')
-            await home_z_axis().run(can_messenger = messenger)
-            input('Press Enter to Drop Tips!!')
+            print('homing Z Axis')
+            await home_z_axis().run(can_messenger=messenger)
+            input('ENTER to DROP-TIPS')
             await drop_tips.run(can_messenger=messenger)
             input('ENTER to home JAW')
             await home_jaw.run(can_messenger=messenger)
-            # await move_y.run(can_messenger = messenger)
-            # await asyncio.sleep(delay)
-            # await move_x.run(can_messenger = messenger)
-            # await asyncio.sleep(delay)
-            # await move_z.run(can_messenger = messenger)
-            # await asyncio.sleep(delay)
-            # await set_current(messenger,1.5)
-            # Home the gear motors and Z before performing the test
-            # await home_jaw.run(can_messenger=messenger)
-            # await asyncio.sleep(delay)
-            # await drop_tips.run(can_messenger=messenger)
-            # await asyncio.sleep(delay)
-            # await home_z.run(can_messenger = messenger)
-            # await asyncio.sleep(delay)
-            # await move_z.run(can_messenger = messenger)
-            # await asyncio.sleep(delay)
-            # await grab_tips.run(can_messenger=messenger)
-            # await asyncio.sleep(delay)
-            # await home_jaw.run(can_messenger=messenger)
-            # await asyncio.sleep(delay)
-            # await home_z.run(can_messenger = messenger)
-            # await asyncio.sleep(delay)
-            # input("press enter to drop tip")
-            # await drop_tips.run(can_messenger=messenger)
-            # await asyncio.sleep(delay)
-            # await home_jaw.run(can_messenger=messenger)
-            # await asyncio.sleep(delay)
 
         elif args.test == 'aspirate/dispense':
             for cycle in range(1, args.cycles+1):
